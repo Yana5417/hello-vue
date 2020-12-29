54b842a3e5e8126b414b4591693401ae5f4a36b0
liyanan <liyanan14631@ipalfish.com>
2020-12-29 14:35:09 +0800

    vue3.0+vant3.0

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <van-button url="/about" type="primary">朴素按钮</van-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {},
+})
+export default class Home extends Vue {}
+</script>
